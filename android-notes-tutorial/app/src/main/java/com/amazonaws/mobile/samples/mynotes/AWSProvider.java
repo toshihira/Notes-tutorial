a511871e7d397eeb77a733bb42e95058ca4e11ba
Toshihira Horikoshi <vx.xb2@gmail.com>
2017-11-19 04:06:01 +0900

    Add Authentication to the Notes App

--- android-notes-tutorial/app/src/main/java/com/amazonaws/mobile/samples/mynotes/AWSProvider.java
+++ android-notes-tutorial/app/src/main/java/com/amazonaws/mobile/samples/mynotes/AWSProvider.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.auth.userpools.CognitoUserPoolsSignInProvider;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.pinpoint.PinpointConfiguration;
 import com.amazonaws.mobileconnectors.pinpoint.PinpointManager;
@@ -30,6 +31,7 @@ public class AWSProvider {
 
         IdentityManager identityManager = new IdentityManager(context, awsConfiguration);
         IdentityManager.setDefaultIdentityManager(identityManager);
+        identityManager.addSignInProvider(CognitoUserPoolsSignInProvider.class);
     }
 
     public Context getContext() {
